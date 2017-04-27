768d01e4adc5f2d0673b6632c725d3f7e5201f80
Raghu Betina <raghu@firstdraft.com>
2017-04-27 23:49:57 +0000

    Direct association between directors and castings

--- app/models/casting.rb
+++ app/models/casting.rb
@@ -1,6 +1,9 @@
 class Casting < ApplicationRecord
   # Direct associations
 
+  belongs_to :movie,
+             :class_name => "Director"
+
   # Indirect associations
 
   # Validations
--- app/models/director.rb
+++ app/models/director.rb
@@ -1,6 +1,10 @@
 class Director < ApplicationRecord
   # Direct associations
 
+  has_many   :castings,
+             :foreign_key => "movie_id",
+             :dependent => :destroy
+
   has_many   :movies,
              :dependent => :destroy
 
